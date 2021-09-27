277c722a49a214a7a4468c801051dfc7f54cc200
ArtemBuch <78083557+ArtemBuch@users.noreply.github.com>
2021-09-27 13:34:49 +0300

    fix header_link

--- js/burger.js
+++ js/burger.js
@@ -1,7 +1,7 @@
 
 // Бургер
 $(document).ready(function() {
-    $('.header__burger, .header__link').click(function(event) {
+    $('.header__burger, .header__list_link').click(function(event) {
         $('.header__burger,.header__menu').toggleClass('active');
         
         $('body').toggleClass('lock_bur');
